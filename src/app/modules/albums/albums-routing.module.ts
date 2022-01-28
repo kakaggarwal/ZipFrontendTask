cf2b2511bb42669a5de0fc63d17a6008b7412bc1
Krishan Aggarwal <kakaggarwal@gmail.com>
2022-01-28 16:55:04 +0530

    DEV-000): Albums module added

---
+++ src/app/modules/albums/albums-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AlbumsComponent } from './pages/albums.component';
+
+const routes: Routes = [{ path: '', component: AlbumsComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AlbumsRoutingModule { }
---
+++ src/app/modules/albums/albums.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AlbumsRoutingModule } from './albums-routing.module';
+import { AlbumsComponent } from './pages/albums.component';
+
+@NgModule({
+  declarations: [
+    AlbumsComponent
+  ],
+  imports: [
+    CommonModule,
+    AlbumsRoutingModule
+  ]
+})
+export class AlbumsModule { }
