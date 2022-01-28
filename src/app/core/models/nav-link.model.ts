5508e9c17ea3d63268cf050614138d2b6d9faf9d
Krishan Aggarwal <kakaggarwal@gmail.com>
2022-01-28 16:59:21 +0530

    NavLink model class created

---
+++ src/app/core/models/nav-link.model.ts
@@ -0,0 +1,9 @@
+export class NavLink {
+    viewValue: string;
+    path: string;
+
+    constructor(viewValue: string, path: string) {
+        this.viewValue = viewValue;
+        this.path = path;
+    }
+}
