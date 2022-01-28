26eee157afda75275126996b681cc2f8c05e17be
Krishan Aggarwal <kakaggarwal@gmail.com>
2022-01-28 16:59:51 +0530

    App Routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'albums', loadChildren: () => import('./modules/albums/albums.module').then(m => m.AlbumsModule) },
+  { path: 'photos', loadChildren: () => import('./modules/photos/photos.module').then(m => m.PhotosModule) },
+  { path: 'posts', loadChildren: () => import('./modules/posts/posts.module').then(m => m.PostsModule) },
+
+  { path: '**', redirectTo: 'dashboard' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
