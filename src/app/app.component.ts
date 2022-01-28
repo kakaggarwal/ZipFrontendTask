b976a474ee017ec31d3161326187a2172ac4f0cf
Krishan Aggarwal <kakaggarwal@gmail.com>
2022-01-28 17:00:01 +0530

    App component UI updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { NavLink } from './core/models/nav-link.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Frontend';
+export class AppComponent implements OnInit {
+
+  links: NavLink[] = [];
+
+  constructor(private title: Title) {
+    this.title.setTitle('Frontend');
+  }
+
+  ngOnInit(): void {
+    this.links = [
+      new NavLink('Dashboard', 'dashboard'),
+      new NavLink('Posts', 'posts'),
+      new NavLink('Albums', 'albums'),
+      new NavLink('Photos', 'photos'),
+    ]
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core/core.module';
+import { MaterialModule } from './shared/material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    MaterialModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
